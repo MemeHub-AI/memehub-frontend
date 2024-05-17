1f81f546114d893d8cb099023f6f6d4d26b08df4
Ce1ling <2134045201@qq.com>
2024-05-18 02:16:02 +0800

    refactor: optimize lang change

--- src/components/lang-select.tsx
+++ src/components/lang-select.tsx
@@ -1,5 +1,6 @@
 import React, { ComponentProps } from 'react'
 import { useTranslation } from 'react-i18next'
+import { GlobeIcon } from '@radix-ui/react-icons'
 
 import {
   Select,
@@ -11,6 +12,13 @@ import {
 import { resources } from '@/i18n'
 import { useLang } from '@/hooks/use-lang'
 import { cn } from '@/lib/utils'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const langs = Object.entries(resources as Record<string, { name: string }>)
 
@@ -18,6 +26,28 @@ export const LangSelect = (props: ComponentProps<'div'>) => {
   const { className } = props
   const { t, i18n } = useTranslation()
   const { setLang } = useLang()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="ml-3">
+          <GlobeIcon width={22} height={22} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {langs.map(([code, { name }], i) => (
+          <DropdownMenuCheckboxItem
+            checked={i18n.language === code}
+            textValue={code}
+            key={i}
+            onClick={() => setLang(code)}
+          >
+            {name}
+          </DropdownMenuCheckboxItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 
   return (
     <Select value={i18n.language} onValueChange={setLang}>
