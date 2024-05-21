d8114f52f1247d3c72c69d9a8a4e0daab464e07a
l1en <2134045201@qq.com>
2024-05-21 21:00:38 +0800

    feat: pagination tokens use query

--- src/hooks/use-tokens.ts
+++ src/hooks/use-tokens.ts
@@ -1,15 +1,25 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
 
 import { tokenApi } from '@/api/token'
 
+enum Default {
+  Page = 1,
+  PageSize = 10,
+}
+
 export const useTokens = () => {
+  const { query, ...router } = useRouter()
+
   const { data } = useInfiniteQuery({
-    initialPageParam: 1,
-    queryKey: [tokenApi.list.name],
+    initialPageParam: Number(query.page) || Default.Page,
+    queryKey: [tokenApi.list.name, router.isReady],
     queryFn: ({ pageParam }) => {
+      if (!router.isReady) return Promise.reject()
+
       return tokenApi.list({
         page: pageParam.toString(),
-        page_size: String(10),
+        page_size: String(query.page_size || Default.PageSize),
       })
     },
     getNextPageParam: (_, __, page) => page + 1,
