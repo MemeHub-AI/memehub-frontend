6a7852dc67650c47a2c5e3a7a4e9714c2f7be63f
l1en <2134045201@qq.com>
2024-05-26 11:11:52 +0800

    fix: fix uniswap v2 sell addr error

--- src/views/token/hooks/use-uniswap-v2.ts
+++ src/views/token/hooks/use-uniswap-v2.ts
@@ -9,8 +9,6 @@ import { customToast } from '@/utils/toast'
 
 const ETH_ADDRESS = '0x5300000000000000000000000000000000000004'
 
-const ETH_RECEIVER = '0x7970af446b850f7Db1229996fff7127795Dc7214'
-
 export const useUniswapV2 = () => {
   const { t } = useTranslation()
   const { address } = useAccount()
@@ -33,19 +31,25 @@ export const useUniswapV2 = () => {
     },
   })
 
-  const uniswapBuy = (amount: string, token: Address) => {
+  const checkForTrade = (amount: string, token: Address) => {
     if (!address || isEmpty(address)) {
       toast.error(t('trade.account.invalid'))
-      return
+      return false
     }
     if (isEmpty(amount)) {
       toast.error(t('trade.amount.invalid'))
-      return
+      return false
     }
     if (!isAddress(token)) {
       toast.error(t('trade.token.invalid'))
-      return
+      return false
     }
+    return true
+  }
+
+  const uniswapBuy = (amount: string, token: Address) => {
+    const isValid = checkForTrade(amount, token)
+    if (!isValid) return
 
     console.log('uniswap buy', amount, token)
     writeContract({
@@ -57,14 +61,8 @@ export const useUniswapV2 = () => {
   }
 
   const uniswapSell = (amount: string, token: Address) => {
-    if (isEmpty(amount)) {
-      toast.error(t('trade.amount.invalid'))
-      return
-    }
-    if (!isAddress(token)) {
-      toast.error(t('trade.token.invalid'))
-      return
-    }
+    const isValid = checkForTrade(amount, token)
+    if (!isValid) return
 
     console.log('uniswap sell', amount, token)
     writeContract({
@@ -74,7 +72,7 @@ export const useUniswapV2 = () => {
         parseEther(amount),
         BigInt(0),
         [token, ETH_ADDRESS],
-        ETH_RECEIVER,
+        address!,
         BigInt(Date.now()),
       ],
     })
