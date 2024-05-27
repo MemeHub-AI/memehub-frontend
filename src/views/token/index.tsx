f613f0cd5dfbc0a3c123bc4a05e03e875d774a4e
l1en <2134045201@qq.com>
2024-05-27 21:32:33 +0800

    feat: add user info to token page

--- src/views/token/index.tsx
+++ src/views/token/index.tsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'react-i18next'
 
 import { TradeTab } from './components/trade-tab'
-import { Button } from '@/components/ui/button'
 import { TokenInfo } from './components/token-info'
 import { CommentTradeTab } from './components/comment-trade-tab'
 import { useResponsive } from '@/hooks/use-responsive'
@@ -11,6 +10,7 @@ import { HoldersRank } from './components/holders-rank'
 import { TokenProvider } from '@/contexts/token'
 import { useTokenInfo } from './hooks/use-token-info'
 import { CandlestickChart } from '@/components/candlestick-chart'
+import { TokenInfoHeader } from './components/token-info-header'
 
 export const TokenPage = () => {
   const { t } = useTranslation()
@@ -21,15 +21,15 @@ export const TokenPage = () => {
   return (
     <TokenProvider value={{ tokenInfo, totalToken, currentToken, refetchInfo }}>
       <main className="px-4 max-sm:px-3 pt-4 max-w-main mx-auto min-h-main">
-        <Button className="mb-3 self-start" onClick={router.back}>
-          {t('back')}
-        </Button>
+        {/* Header */}
+        <TokenInfoHeader />
+
+        {/* Body */}
         <div className="flex space-x-4 max-sm:flex-col max-sm:space-x-0 mt-4">
           {/* Left */}
           <div className="flex flex-col flex-1">
             {isMobile && <TradeTab />}
             <CandlestickChart />
-            {/* <TokenInfo className="mt-0" /> */}
             <CommentTradeTab className="my-6 max-sm:mb-0" />
           </div>
 
@@ -37,6 +37,7 @@ export const TokenPage = () => {
           {!isMobile && (
             <div className="w-aside">
               <TradeTab />
+              <TokenInfo />
               <HoldersRank className="mt-4" />
             </div>
           )}
