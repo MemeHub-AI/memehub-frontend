10f0447fdad42acd9e55d204e20d578066a4a4dc
l1en <2134045201@qq.com>
2024-05-30 17:44:48 +0800

    refactor: optimize use-upload-image

--- src/hooks/use-upload-image.ts
+++ src/hooks/use-upload-image.ts
@@ -1,35 +1,44 @@
 import React, { useState } from 'react'
-import { first, isEmpty } from 'lodash'
+import { first } from 'lodash'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import { useTranslation } from 'react-i18next'
 
-import type { ApiResponse } from '@/api/types'
-
 import { otherApi } from '@/api/other'
-import { useStorage } from './use-storage'
 
 interface Options {
   onSuccess?: (url: string) => void
-  onErrror?: (reason: string) => void
+  onError?: (reason: string) => void
   onFinally?: () => void
 }
 
 const MAX_KB = 300
 
 export const useUploadImage = (options?: Options) => {
-  const { onSuccess, onErrror, onFinally } = options || {}
+  const { onSuccess, onError, onFinally } = options || {}
   const { t } = useTranslation()
-  const [url, setUrl] = useState('')
   const [file, setFile] = useState<File | null>(null)
-  const { getToken } = useStorage()
-
-  const { mutateAsync } = useMutation({
-    mutationKey: [otherApi.uploadImage.name],
-    mutationFn: otherApi.uploadImage,
-  })
 
   const clearFile = () => setFile(null)
+
+  const { data, mutateAsync } = useMutation({
+    mutationKey: [otherApi.uploadImage.name],
+    mutationFn: otherApi.uploadImage,
+    onMutate: () => toast.loading(t('uploading')),
+    onSettled: (_, __, ___, id) => {
+      toast.dismiss(id)
+      onFinally?.()
+    },
+    onError: (e) => {
+      toast.error(t('upload.failed'))
+      onError?.(e.message)
+      clearFile()
+    },
+    onSuccess: ({ data }) => {
+      toast.success(t('upload.success'))
+      onSuccess?.(data.image_url)
+    },
+  })
 
   const onChangeUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = first(e.target.files)!
@@ -43,45 +52,11 @@ export const useUploadImage = (options?: Options) => {
 
     formData.append('avatar', file)
     setFile(file)
-
-    const id = toast.loading(t('uploading'))
-    try {
-      const headers = new Headers()
-      const token = getToken() || ''
-      if (!isEmpty(token)) {
-        headers.append('Authorization', `Bearer ${token}`)
-      }
-      const response = await fetch('https://api.memehub.ai/api/v1/upload/', {
-        method: 'POST',
-        body: formData,
-        headers,
-      })
-
-      if (!response.ok) {
-        console.error('Failed to upload image')
-        return
-      }
-
-      const { data } = (await response.json()) as ApiResponse<{
-        image_url: string
-      }>
-
-      setUrl(data.image_url)
-      onSuccess?.(data.image_url)
-      toast.success(t('upload.success'))
-    } catch (error) {
-      console.error(error)
-      clearFile()
-      onErrror?.(String(error))
-      toast.success(t('upload.failed'))
-    } finally {
-      toast.dismiss(id)
-      onFinally?.()
-    }
+    mutateAsync(formData)
   }
 
   return {
-    url,
+    url: data?.data.image_url ?? '',
     file,
     onChangeUpload,
     clearFile,
