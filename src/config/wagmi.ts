a4b02e2759063b5d26bc5f41a927d1da812501af
Leo <linleo876@gmail.com>
2024-05-30 15:39:28 +0800

    chore: optimize wagmi config & contract address management

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,38 +1,40 @@
 import { http, createConfig, fallback, unstable_connector } from 'wagmi'
 import { injected } from 'wagmi/connectors'
 import {
-  scroll,
-  scrollSepolia,
   mainnet,
   bsc,
+  opBNB,
+  scroll,
+  sepolia,
   bscTestnet,
   opBNBTestnet,
+  scrollSepolia,
 } from 'wagmi/chains'
-import { first } from 'lodash'
 
-// const prod = createConfig({
-//   chains: [bscTestnet, bsc, scroll, mainnet],
-//   connectors: [injected()],
-//   transports: {
-//     [bscTestnet.id]: fallback([http(), unstable_connector(injected)]),
-//     [bsc.id]: fallback([http(), unstable_connector(injected)]),
-//     [scroll.id]: fallback([http(), unstable_connector(injected)]),
-//     [mainnet.id]: fallback([http(), unstable_connector(injected)]),
-//   },
-//   ssr: true,
-// })
-
-const dev = createConfig({
-  chains: [scrollSepolia, bscTestnet, opBNBTestnet],
+const prod = createConfig({
+  chains: [mainnet, bsc, opBNB, scroll],
   connectors: [injected()],
   transports: {
-    [scrollSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [bscTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    [opBNBTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [mainnet.id]: fallback([http(), unstable_connector(injected)]),
+    [bsc.id]: fallback([http(), unstable_connector(injected)]),
+    [opBNB.id]: fallback([http(), unstable_connector(injected)]),
+    [scroll.id]: fallback([http(), unstable_connector(injected)]),
   },
   ssr: true,
 })
 
-export const wagmiConfig = dev
+const dev = createConfig({
+  chains: [sepolia, bscTestnet, opBNBTestnet, scrollSepolia, ...prod.chains],
+  connectors: [injected()],
+  transports: {
+    [sepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [bscTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [opBNBTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [scrollSepolia.id]: fallback([http(), unstable_connector(injected)]),
+    // includes mainnet.
+    ...prod._internal.transports,
+  },
+  ssr: true,
+})
 
-export const chain = first(wagmiConfig.chains)!
+export const wagmiConfig = process.env.NODE_ENV === 'production' ? prod : dev
--- src/contract/address.ts
+++ src/contract/address.ts
@@ -1,13 +1,44 @@
-// dev
-export const uniswapV2Address = '0x9B3336186a38E1b6c21955d112dbb0343Ee061eE'
-export const factoryAddress = {
-  scroll: '0x35Ce38AC48Dd3c7Bf6bd14dE8e81128d76E11885',
-  bsc: '0x9d51006F4511b5D9B05e443FBb29285e14092CAC',
-  opBsc: '0xC60B0Fc8Ada34C056c7DE59044de8Db12508877e',
-} as const
+import {
+  sepolia,
+  bscTestnet,
+  opBNBTestnet,
+  scrollSepolia,
+  mainnet,
+  bsc,
+  opBNB,
+  scroll,
+} from '@wagmi/core/chains'
 
 export const idoAddress = '0x1778EDfF5DBD9a64Fa54D237F828F317DB884056'
 
-// prod
-
 export const zeroAddress = '0x0000000000000000000000000000000000000000'
+
+const prod = {
+  factory: {
+    // only mainnet
+    [mainnet.id]: '',
+    [bsc.id]: '0x088F3DEb771A6a7608b6FBd5715708F5957F63fD',
+    [opBNB.id]: '',
+    [scroll.id]: '0x35Ce38AC48Dd3c7Bf6bd14dE8e81128d76E11885',
+  },
+  uniswapV2: '0x9B3336186a38E1b6c21955d112dbb0343Ee061eE',
+} as const
+
+const dev = {
+  factory: {
+    // testnet
+    [sepolia.id]: '',
+    [bscTestnet.id]: '0x5d562bC8Ae3bA73d57AaBE2312a65aFEcb743edd',
+    [opBNBTestnet.id]: '',
+    [scrollSepolia.id]: '',
+
+    // mainnet
+    [mainnet.id]: '',
+    [bsc.id]: '',
+    [opBNB.id]: '',
+    [scroll.id]: '0x34EeA145539327499A00B499F05b6e8eD3FdCbdc',
+  },
+  uniswapV2: '0x9B3336186a38E1b6c21955d112dbb0343Ee061eE',
+} as const
+
+export const ca = process.env.NODE_ENV === 'production' ? prod : dev
