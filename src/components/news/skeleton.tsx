616ee7df92292e8076d41332ae3eb4337b7a38a7
l1en <2134045201@qq.com>
2024-06-01 15:11:57 +0800

    refactor: optimize skeleton for homepage

--- src/components/news/skeleton.tsx
+++ src/components/news/skeleton.tsx
@@ -2,16 +2,16 @@ import { Skeleton } from '../ui/skeleton'
 
 export const NewsSkeleton = () => {
   return (
-    <div className="flex flex-col gap-6 h-[calc(100vh-260px)] overflow-y-auto max-md:h-[unset] max-md:gap-4 max-md:overflow-y-clip">
+    <div className="flex flex-col gap-6 h-[calc(100vh-260px)] overflow-y-auto max-md:h-[unset] max-md:gap-4 max-md:overflow-y-clip pr-6">
       {Array.from({ length: 5 }).map((_, i) => (
-        <div key={i} className="flex gap-2 w-full">
-          <Skeleton className="h-[90px] w-[90px] rounded-lg" />
+        <div key={i} className="flex gap-2 w-full border-2 rounded-lg p-2">
+          <Skeleton className="h-20 w-20 rounded-lg" />
           <div className="flex flex-col justify-between flex-1">
-            <Skeleton className="h-6 w-28 rounded-lg" />
             <div className="flex flex-col gap-2">
               <Skeleton className="h-4 w-full rounded-lg" />
-              <Skeleton className="h-4 w-1/2 rounded-lg" />
+              <Skeleton className="h-4 w-1/3 rounded-lg" />
             </div>
+            <Skeleton className="h-7 w-20 rounded-lg" />
           </div>
         </div>
       ))}
