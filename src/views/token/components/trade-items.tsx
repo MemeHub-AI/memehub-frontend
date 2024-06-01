6542adc60fe601b05616b6fdb38bad92e67fdc9a
l1en <2134045201@qq.com>
2024-06-01 21:17:22 +0800

    fix: open connect for trade items

--- src/views/token/components/trade-items.tsx
+++ src/views/token/components/trade-items.tsx
@@ -1,8 +1,10 @@
 import React, { ComponentProps } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useAccount } from 'wagmi'
 
 import { Button } from '@/components/ui/button'
 import { useTradeContext } from '@/contexts/trade'
+import { useWalletStore } from '@/stores/use-wallet-store'
 
 const buyItems = ['0.001', '0.01', '1']
 
@@ -18,6 +20,8 @@ export const TradeItems = (props: Props) => {
   const { disabled, onBuyItemClick, onSellItemClick, onResetClick } = props
   const { t } = useTranslation()
   const { isBuy, nativeSymbol } = useTradeContext()
+  const { isConnected } = useAccount()
+  const { setConnectOpen } = useWalletStore()
 
   return (
     <div className="flex gap-2 mt-3">
@@ -29,6 +33,10 @@ export const TradeItems = (props: Props) => {
           size="xs"
           key={i}
           onClick={() => {
+            if (!isConnected) {
+              setConnectOpen(true)
+              return
+            }
             isBuy ? onBuyItemClick?.(value) : onSellItemClick?.(value)
           }}
           disabled={disabled}
