09969f68a5d2188c50b12a2c08cf5b70f4f553f7
l1en <2134045201@qq.com>
2024-06-02 17:00:45 +0800

    feat: add auth for ai generate

--- src/api/ai/index.ts
+++ src/api/ai/index.ts
@@ -1,3 +1,4 @@
+import { CommonHeaders } from '@/hooks/use-fetch'
 import { api } from '..'
 import { ApiResponse } from '../types'
 import {
@@ -7,11 +8,16 @@ import {
   AIMemePosterQuery,
 } from './type'
 
+const headers = {
+  [CommonHeaders.Authorization]: 'Bearer eyJ0eXAiOiJqd3QiLCJhbGciOiJIUzI1NiJ9',
+}
+
 export const aiApi = {
   getMemeInfo: async (data?: AIMemeInfoQuery, signal?: AbortSignal) => {
     return api.POST<ApiResponse<AIMemeInfo>>('/ai/meme-info', {
       body: data,
       signal: signal,
+      headers,
     })
   },
 
@@ -19,6 +25,7 @@ export const aiApi = {
     return api.POST<ApiResponse<string[]>>('/ai/meme-logo', {
       body: data,
       signal: signal,
+      headers,
     })
   },
 
@@ -26,6 +33,7 @@ export const aiApi = {
     return api.POST<ApiResponse<AIEMemePosterData>>('/ai/meme-poster', {
       body: data,
       signal: signal,
+      headers,
     })
   },
 }
