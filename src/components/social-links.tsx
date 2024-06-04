4f2154cf42f42b03d19cd4a5ccd6c65e96d54dc8
l1en <2134045201@qq.com>
2024-06-04 16:20:49 +0800

    fix: social link icon

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,19 +1,20 @@
 import React, { ComponentProps } from 'react'
 import { Send, Twitter } from 'lucide-react'
+import { VariantProps } from 'class-variance-authority'
+import { useTranslation } from 'react-i18next'
 
 import { Button, buttonVariants } from './ui/button'
 import { cn } from '@/lib/utils'
-import { VariantProps } from 'class-variance-authority'
-import { useTranslation } from 'react-i18next'
 
 export const SocialLinks = ({
   className,
   size = 'icon',
 }: ComponentProps<'div'> & VariantProps<typeof buttonVariants>) => {
   const { t } = useTranslation()
+
   const links = [
-    { name: t('twitter-x'), icon: <Send strokeWidth={1.5} size={21} /> },
-    { name: t('telegram'), icon: <Twitter strokeWidth={1.5} size={22} /> },
+    { name: t('twitter-x'), icon: <Twitter strokeWidth={1.5} size={22} /> },
+    { name: t('telegram'), icon: <Send strokeWidth={1.5} size={21} /> },
   ]
 
   return (
