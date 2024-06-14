52f4a9236f0f2b5e33950bb9bf0ef7facb0e6f6a
l1en <2134045201@qq.com>
2024-06-14 23:19:32 +0800

    feat: update user profile

--- src/components/reward-button.tsx
+++ src/components/reward-button.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { ComponentProps } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
 
 import { Button } from './ui/button'
 import { Routes } from '@/routes'
+import { cn } from '@/lib/utils'
 
-export const RewardButton = () => {
+interface Props extends ComponentProps<typeof Button> {
+  showReferral?: boolean
+}
+
+export const RewardButton = ({
+  showReferral = false,
+  className,
+  ...props
+}: Props) => {
   const { t } = useTranslation()
   const router = useRouter()
 
   return (
     <Button
       variant="outline"
-      className="bg-lime-300 text-blue-deep font-bold gap-2"
+      className={cn(
+        'bg-lime-300 text-blue-deep hover:bg-lime-500 font-bold gap-2',
+        className
+      )}
       onClick={() => router.push(Routes.Reward)}
+      {...props}
     >
-      {/* <div>🧑‍🚀 {t('referral')}</div> */}
+      {showReferral && <div>🧑‍🚀 {t('referral')}</div>}
       <div>💎 {t('diamond')}</div>
     </Button>
   )
