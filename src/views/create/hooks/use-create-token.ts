92147e95f98562eb18aacf53424fe0671a589349
l1en <2134045201@qq.com>
2024-06-24 14:52:38 +0800

    feat: dynamic params for create token

--- src/views/create/hooks/use-create-token.ts
+++ src/views/create/hooks/use-create-token.ts
@@ -1,17 +1,8 @@
 import { useMutation } from '@tanstack/react-query'
-import { isEmpty } from 'lodash'
-import { BigNumber } from 'bignumber.js'
 
-import {
-  MarketType,
-  type Marketing,
-  type TokenUpdateReq,
-} from '@/api/token/types'
-
+import type { TokenUpdateReq } from '@/api/token/types'
 import { tokenApi } from '@/api/token'
 import { airdropApi } from '@/api/airdrop'
-import { AirdropMerkleRootRes } from '@/api/airdrop/types'
-import { bondDistributorParams } from '@/contract/v2/config/bond'
 
 export const useCreateToken = () => {
   // Submit created token to backend.
@@ -51,59 +42,6 @@ export const useCreateToken = () => {
     mutationFn: airdropApi.getMerkleRoot,
   })
 
-  const parsePercent = (p: number) => BigNumber(p).multipliedBy(100).toNumber()
-
-  const updateAirdropParams = (
-    data: AirdropMerkleRootRes,
-    params: typeof bondDistributorParams,
-    marketing: Marketing[]
-  ) => {
-    const { kol_count, kol_root_hash, community_count, community_root_hash } =
-      data
-    // A new object must be used.
-    const p = { ...params } as typeof bondDistributorParams
-
-    const kol = marketing.find((m) => m.type === MarketType.Kol)
-    const cmnt = marketing.find((m) => m.type === MarketType.Community)
-
-    // Is kol airdrop.
-    if (kol_count && kol_root_hash && kol) {
-      p.distributionRatioKol = parsePercent(kol.percent)
-      p.walletCountKol = kol_count
-      p.merkleRootKol = kol_root_hash
-    }
-
-    // Is community airdrop.
-    if (community_count && community_root_hash && cmnt) {
-      p.distributionRatioCommunity = parsePercent(cmnt.percent)
-      p.walletCountCommunity = community_count
-      p.merkleRootCommunity = community_root_hash
-    }
-
-    return p
-  }
-
-  const getAirdropParams = async (
-    chain: string,
-    marketing: Marketing[] | undefined
-  ) => {
-    const type_list = marketing?.map((m) => m.type).join(',') ?? ''
-
-    // Didn't select marketing.
-    if (!marketing || isEmpty(type_list)) return bondDistributorParams
-
-    try {
-      const { data } = await getMerkleRoot({ chain, type_list })
-      if (data) {
-        return updateAirdropParams(data, bondDistributorParams, marketing)
-      }
-
-      return bondDistributorParams
-    } catch (error) {
-      return
-    }
-  }
-
   return {
     createTokenData,
     updateTokenData,
@@ -117,6 +55,5 @@ export const useCreateToken = () => {
     create,
     update,
     getMerkleRoot,
-    getAirdropParams,
   }
 }
--- src/views/create/hooks/use-deploy-v2.ts
+++ src/views/create/hooks/use-deploy-v2.ts
@@ -1,18 +1,75 @@
 import { Config } from 'wagmi'
 import { WriteContractMutate } from 'wagmi/query'
+import { BigNumber } from 'bignumber.js'
+import { isEmpty } from 'lodash'
 
 import type { DeployParams } from './use-deploy'
 import { useCreateToken } from './use-create-token'
-import { getBondConfig } from '@/contract/v2/config/bond'
+import { bondDistributorParams, getBondConfig } from '@/contract/v2/config/bond'
 import { CONTRACT_ERR } from '@/errors/contract'
 import { useChainInfo } from '@/hooks/use-chain-info'
 import { DEPLOY_FEE } from '@/constants/contract'
+import { AirdropMerkleRootRes } from '@/api/airdrop/types'
+import { MarketType, Marketing } from '@/api/token/types'
 
 export const useDeployV2 = (
   writeContract: WriteContractMutate<Config, unknown>
 ) => {
   const { chainId, chainName } = useChainInfo()
-  const { getAirdropParams } = useCreateToken()
+  const { getMerkleRoot } = useCreateToken()
+
+  const parsePercent = (p: number) => BigNumber(p).multipliedBy(100).toNumber()
+
+  const updateAirdropParams = (
+    data: AirdropMerkleRootRes,
+    params: typeof bondDistributorParams,
+    marketing: Marketing[]
+  ) => {
+    const { kol_count, kol_root_hash, community_count, community_root_hash } =
+      data
+    // A new object must be used.
+    const p = { ...params } as typeof bondDistributorParams
+
+    const kol = marketing.find((m) => m.type === MarketType.Kol)
+    const cmnt = marketing.find((m) => m.type === MarketType.Community)
+
+    // Is kol airdrop.
+    if (kol_count && kol_root_hash && kol) {
+      p.distributionRatioKol = parsePercent(kol.percent)
+      p.walletCountKol = kol_count
+      p.merkleRootKol = kol_root_hash
+    }
+
+    // Is community airdrop.
+    if (community_count && community_root_hash && cmnt) {
+      p.distributionRatioCommunity = parsePercent(cmnt.percent)
+      p.walletCountCommunity = community_count
+      p.merkleRootCommunity = community_root_hash
+    }
+
+    return p
+  }
+
+  const getAirdropParams = async (
+    chain: string,
+    marketing: Marketing[] | undefined
+  ) => {
+    const type_list = marketing?.map((m) => m.type).join(',') ?? ''
+
+    // Didn't select marketing.
+    if (!marketing || isEmpty(type_list)) return bondDistributorParams
+
+    try {
+      const { data } = await getMerkleRoot({ chain, type_list })
+      if (data) {
+        return updateAirdropParams(data, bondDistributorParams, marketing)
+      }
+
+      return bondDistributorParams
+    } catch (error) {
+      return
+    }
+  }
 
   const deployV2 = async ({
     name,
