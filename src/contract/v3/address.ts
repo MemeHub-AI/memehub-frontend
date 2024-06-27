85a7aaaa183b10537d99c14c439b29c50d6183a3
l1en <2134045201@qq.com>
2024-06-27 17:03:24 +0800

    chore: udpate contract addr

--- src/contract/v3/address.ts
+++ src/contract/v3/address.ts
@@ -1,19 +1,15 @@
 import { bscTestnet, opBNBTestnet } from 'wagmi/chains'
 
-// bondingCurve: '0x1675F736bBB3bD68A26bDE0B7AfE15316CD2E627',
-// distributor: '0x0A5D954f336933481b3EBe3d88c059b36Fd6609d',
-// recommend: '0x3821885f067d359c3c409e450910eE1B029b2c62',
-
 const dev = {
   [bscTestnet.id]: {
-    bondingCurve: '0x5b0325204B0Aa0E14DbB1c202913bF3ab679E79B',
-    distributor: '0x27786aF25a5f4363c287EeF3E6ae917Be5611f4e',
-    recommend: '0x01c2ADcf1Fac9e31E3b9c8D0e39B537D7CBb7e86',
+    bondingCurve: '0x095BB78B3e674e32480CAD017c4D5206348De613',
+    distributor: '0xC89fD95C324e612433A2FA60A51902C2c36D698E',
+    recommend: '0x6b070198e58A82fBE39CD9eD53e38edB59d6e4Ef',
   },
   [opBNBTestnet.id]: {
-    bondingCurve: '0x16238150eFd5b9490157f87904A08B4caec0ee27',
-    distributor: '0x21F5C03F8Db42C8e43917C5902A996CD1189eA5A',
-    recommend: '0x71f6239b713f9Fdc2D24e73DB6230535b9d4964c',
+    bondingCurve: '0x2130d664afF9F3EDdAfeCc1a8bA7D33CB28cE557',
+    distributor: '0x15cB7b8997484aD75683043b41f6A18B41d87A4F',
+    recommend: '0xcf40E990a4ec2E1a3601839c7dD3b3C111AC442b',
   },
 } as const
 
