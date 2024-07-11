72c97762f6248bc93633f160f1feb6e4494119f0
Leo <1909802953@qq.com>
2024-07-11 21:09:33 +0800

    feat: i18n name

--- src/components/ambassador-card/index.tsx
+++ src/components/ambassador-card/index.tsx
@@ -2,6 +2,7 @@ import { defaultImg } from '@/config/link'
 import { Card } from '../ui/card'
 import { Img } from '../img'
 import { URL_TYPE, utilsUrl } from '@/utils/url'
+import { utilLang } from '@/utils/lang'
 
 interface AmbassadorCardProps {
   data?: {
@@ -34,12 +35,15 @@ export const AmbassadorCard = ({ data }: AmbassadorCardProps) => {
       <div className="ml-2">
         <h2
           className="text-2xl max-sm:text-xl break-all line-clamp-2"
-          title={data?.name}
+          title={utilLang.getContent(data?.name)}
         >
-          {data?.name}
+          {utilLang.getContent(data?.name)}
         </h2>
-        <div className="mt-2 break-all line-clamp-2" title={data?.description}>
-          {data?.description}
+        <div
+          className="mt-2 break-all line-clamp-2"
+          title={utilLang.getContent(data?.description)}
+        >
+          {utilLang.getContent(data?.description)}
         </div>
       </div>
     </Card>
