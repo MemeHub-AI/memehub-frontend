e6dbcd2de95bab45129516064f35295f2d18d2cd
l1en <2134045201@qq.com>
2024-07-27 19:44:22 +0800

    feat: prod ido airdrop

--- src/views/airdrop/components/ido-card.tsx
+++ src/views/airdrop/components/ido-card.tsx
@@ -51,9 +51,9 @@ export const IdoAirdropCard = ({
   } = useIdoCommunityAirdrop(!isKolAirdrop)
   const { isKol, isCommunity } = useIdoCheck(idoChain.id)
 
-  const amount = isKolAirdrop ? 6666 ?? kolAmount : 3000 ?? communityAmount
-  const current = isKolAirdrop ? 0 ?? kolCurrent : 0 ?? communityCurrent
-  const total = isKolAirdrop ? 300 ?? kolTotal : 2000 ?? communityTotal
+  const amount = isKolAirdrop ? kolAmount : communityAmount
+  const current = isKolAirdrop ? kolCurrent : communityCurrent
+  const total = isKolAirdrop ? kolTotal : communityTotal
   const isClaimed = isKolAirdrop ? isKolClaimed : isCommunityClaimed
   const hasId = isKolAirdrop ? isKol : isCommunity
 
@@ -73,7 +73,6 @@ export const IdoAirdropCard = ({
 
   // TODO: remove `true`
   const disabled =
-    true ||
     isClaming ||
     isClaimed ||
     BigNumber(amount).isZero() ||
@@ -81,8 +80,7 @@ export const IdoAirdropCard = ({
     (!isKolAirdrop && isBelowThreshold)
 
   const renderButtonText = () => {
-    // TODO: remove
-    return t('ido.airdrop.not-start')
+    if (isNotStart) return t('ido.airdrop.not-start')
 
     if (isClaming) return t('airdrop.claiming')
 
