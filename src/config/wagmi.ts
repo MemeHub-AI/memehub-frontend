de0975d93f381effc2238d235f0edf8242e77019
l1en <2134045201@qq.com>
2024-07-30 16:02:17 +0800

    refactor: wagmi config

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -9,6 +9,8 @@ import {
   blast,
   fantom,
   zkSync,
+
+  // testnet
   sepolia,
   bscTestnet,
   opBNBTestnet,
@@ -18,11 +20,45 @@ import {
   fantomTestnet,
   zkSyncSepoliaTestnet,
 } from 'wagmi/chains'
-
 import { dotenv } from '@/utils/env'
 
-const prod = createConfig({
-  chains: [mainnet, bsc, opBNB, scroll, base, blast, fantom, zkSync],
+const dev = {
+  chains: [
+    sepolia,
+    bscTestnet,
+    opBNBTestnet,
+    scrollSepolia,
+    baseSepolia,
+    blastSepolia,
+    fantomTestnet,
+    zkSyncSepoliaTestnet,
+  ],
+  transports: {
+    [sepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [bscTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [opBNBTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [scrollSepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [baseSepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [blastSepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [fantomTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [zkSyncSepoliaTestnet.id]: fallback([http(), unstable_connector(injected)]),
+  },
+}
+
+export const wagmiConfig = createConfig({
+  chains: [
+    mainnet,
+    bsc,
+    base,
+    blast,
+    opBNB,
+    scroll,
+    fantom,
+    zkSync,
+
+    // testnet
+    ...(dotenv.isDev ? dev.chains : []),
+  ],
   connectors: [injected()],
   transports: {
     [mainnet.id]: fallback([http(), unstable_connector(injected)]),
@@ -33,8 +69,6 @@ const prod = createConfig({
       http(),
       unstable_connector(injected),
     ]),
-    [opBNB.id]: fallback([http(), unstable_connector(injected)]),
-    [scroll.id]: fallback([http(), unstable_connector(injected)]),
     [base.id]: fallback([
       http(
         'https://proportionate-skilled-dinghy.base-mainnet.quiknode.pro/09c165cec4358bc65dbb5695ceb663741662b638/'
@@ -49,45 +83,15 @@ const prod = createConfig({
       http(),
       unstable_connector(injected),
     ]),
+    [opBNB.id]: fallback([http(), unstable_connector(injected)]),
+    [scroll.id]: fallback([http(), unstable_connector(injected)]),
     [fantom.id]: fallback([http(), unstable_connector(injected)]),
     [zkSync.id]: fallback([http(), unstable_connector(injected)]),
+
+    // testnet
+    ...(dotenv.isDev ? dev.transports : ({} as typeof dev.transports)),
   },
   ssr: true,
 })
-
-const dev = createConfig({
-  chains: [
-    sepolia,
-    bscTestnet,
-    opBNBTestnet,
-    scrollSepolia,
-    baseSepolia,
-    blastSepolia,
-    fantomTestnet,
-    zkSyncSepoliaTestnet,
-    ...prod.chains,
-  ],
-  connectors: [injected()],
-  transports: {
-    [sepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [bscTestnet.id]: fallback([
-      http(
-        'https://greatest-broken-tab.bsc-testnet.quiknode.pro/0ce6bb713784c756397169ec4fc2fa6f7eaa9608'
-      ),
-      unstable_connector(injected),
-    ]),
-    [opBNBTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    [scrollSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [baseSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [blastSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [fantomTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    [zkSyncSepoliaTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    // includes mainnet.
-    ...prod._internal.transports,
-  },
-  ssr: true,
-})
-
-export const wagmiConfig = dotenv.isProd ? prod : dev
 
 export type ChainId = (typeof wagmiConfig)['chains'][number]['id']
