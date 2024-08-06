c9645545c94b99d01a8ed33eac0855263e3a8e89
qhcy <2407952148@qq.com>
2024-08-06 23:01:09 +0800

    feat: wallet connection

--- src/components/connect-wallet.tsx
+++ src/components/connect-wallet.tsx
@@ -1,37 +1,38 @@
 import React, { type ComponentProps } from 'react'
 import { useAccount } from 'wagmi'
-import { useTranslation } from 'react-i18next'
-import { useConnectModal } from '@rainbow-me/rainbowkit'
 
 import { Button } from './ui/button'
-import { useResponsive } from '@/hooks/use-responsive'
+import { ChangeChainWallets } from './header/change-chain-wallets'
+import { useTonConnectUI } from '@tonconnect/ui-react'
 
 export const ConnectWallet = ({
   children,
   className,
 }: ComponentProps<typeof Button>) => {
-  const { t } = useTranslation()
-  const { isConnected, isConnecting } = useAccount()
-  const { openConnectModal } = useConnectModal()
-  const { isMobile } = useResponsive()
+  const { isConnected } = useAccount()
+  const [tonConnectUI, setOptions] = useTonConnectUI()
 
   return (
     <>
-      {isConnected ? (
+      {isConnected || tonConnectUI.account?.address ? (
         children
       ) : (
-        <Button
-          className={className}
-          size={isMobile ? 'sm' : 'default'}
-          disabled={isConnecting}
-          onClick={() => openConnectModal?.()}
-          type="button"
-        >
-          {isConnecting ? t('wallet.connecting') : t('wallet.connect')}
-        </Button>
+        <ChangeChainWallets className={className} />
       )}
     </>
   )
 }
 
 export default ConnectWallet
+
+/**
+  <Button
+    className={className}
+    size={isMobile ? 'sm' : 'default'}
+    disabled={isConnecting}
+    onClick={() => openConnectModal?.()}
+    type="button"
+  >
+    {isConnecting ? t('wallet.connecting') : t('wallet.connect')}
+  </Button>
+ */
