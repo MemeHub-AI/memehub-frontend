1464a26c2871b776c6f707fa01d6089157d383a1
yyyhakuna <yjx020714@icloud.com>
2024-08-09 23:45:21 +0800

    feat: sol

--- src/hooks/use-wallet.ts
+++ src/hooks/use-wallet.ts
@@ -1,11 +1,26 @@
 import { useTonAddress } from '@tonconnect/ui-react'
 import { useAccount } from 'wagmi'
+// import { Wallet } from '@solana/wallet-adapter-react'
 
 export const useWallet = () => {
   const tonAddress = useTonAddress()
   const { address } = useAccount()
 
-  const walletAddress = tonAddress || address
+  const walletAddress = () => {
+    if (tonAddress !== '') {
+      return tonAddress
+    }
+
+    if (address) {
+      return address
+    }
+
+    if (publicKey) {
+      return publicKey.toString()
+    }
+
+    return 'no wallet'
+  }
 
   return { walletAddress }
 }
