2bf1dbfccae836fd0a4b7bd617fa42378888a5ee
l1en <2134045201@qq.com>
2024-08-20 23:49:49 +0800

    chore: add memex abi versionc

---
+++ src/contract/abi/memex/factory/index.ts
@@ -0,0 +1,7 @@
+import { memexFactoryAbi0_1_15 } from './0.1.15'
+
+export const memexFactoryAbiMap = {
+  '0.1.15': memexFactoryAbi0_1_15,
+}
+
+export type MemexFactoryVersion = keyof typeof memexFactoryAbiMap
---
+++ src/contract/abi/memex/ido/index.ts
@@ -0,0 +1,7 @@
+import { memexIdoAbi0_1_15 } from './0.1.15'
+
+export const memexIdoAbiMap = {
+  '0.1.15': memexIdoAbi0_1_15,
+}
+
+export type MemexIdoVersion = keyof typeof memexIdoAbiMap
