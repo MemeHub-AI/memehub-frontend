ae1b963ce503d3745b40d0f9c65d6225e8fe83c7
l1en <2134045201@qq.com>
2024-08-22 21:55:51 +0800

    chore: wagmi transports config

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -35,20 +35,20 @@ const dev = {
     zkSyncSepoliaTestnet,
   ],
   transports: {
-    [sepolia.id]: fallback([http(), unstable_connector(injected)]),
+    [sepolia.id]: fallback([unstable_connector(injected), http()]),
     [bscTestnet.id]: fallback([
       http(
         'https://polished-old-valley.bsc-testnet.quiknode.pro/af2b2e4daee6f862aaff645c537377f3d4605d79/'
       ),
-      http(),
       unstable_connector(injected),
+      http(),
     ]),
-    [opBNBTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    [scrollSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [baseSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [blastSepolia.id]: fallback([http(), unstable_connector(injected)]),
-    [fantomTestnet.id]: fallback([http(), unstable_connector(injected)]),
-    [zkSyncSepoliaTestnet.id]: fallback([http(), unstable_connector(injected)]),
+    [opBNBTestnet.id]: fallback([unstable_connector(injected), http()]),
+    [scrollSepolia.id]: fallback([unstable_connector(injected), http()]),
+    [baseSepolia.id]: fallback([unstable_connector(injected), http()]),
+    [blastSepolia.id]: fallback([unstable_connector(injected), http()]),
+    [fantomTestnet.id]: fallback([unstable_connector(injected), http()]),
+    [zkSyncSepoliaTestnet.id]: fallback([unstable_connector(injected), http()]),
   },
 }
 
@@ -73,32 +73,32 @@ export const wagmiConfig = getDefaultConfig({
     ...(dotenv.isDev ? dev.chains : []),
   ],
   transports: {
-    [mainnet.id]: fallback([http(), unstable_connector(injected)]),
+    [mainnet.id]: fallback([unstable_connector(injected), http()]),
     [bsc.id]: fallback([
       http(
         'https://smart-burned-patron.bsc.quiknode.pro/d8340a43ded2474d18828922b6df34efc4d4f190/'
       ),
-      http(),
       unstable_connector(injected),
+      http(),
     ]),
     [base.id]: fallback([
       http(
         'https://proportionate-skilled-dinghy.base-mainnet.quiknode.pro/09c165cec4358bc65dbb5695ceb663741662b638/'
       ),
-      http(),
       unstable_connector(injected),
+      http(),
     ]),
     [blast.id]: fallback([
       http(
         'https://maximum-autumn-pallet.blast-mainnet.quiknode.pro/8e19e2cff98f32361895ec231925b109b0f99a92/'
       ),
-      http(),
       unstable_connector(injected),
+      http(),
     ]),
-    [opBNB.id]: fallback([http(), unstable_connector(injected)]),
-    [scroll.id]: fallback([http(), unstable_connector(injected)]),
-    [fantom.id]: fallback([http(), unstable_connector(injected)]),
-    [zkSync.id]: fallback([http(), unstable_connector(injected)]),
+    [opBNB.id]: fallback([unstable_connector(injected), http()]),
+    [scroll.id]: fallback([unstable_connector(injected), http()]),
+    [fantom.id]: fallback([unstable_connector(injected), http()]),
+    [zkSync.id]: fallback([unstable_connector(injected), http()]),
 
     // testnet
     ...(dotenv.isDev ? dev.transports : ({} as typeof dev.transports)),
