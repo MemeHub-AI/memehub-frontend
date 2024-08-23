cd7673449ac094d525e1bf2325fd2829c4a44f45
l1en <2134045201@qq.com>
2024-08-23 21:01:04 +0800

    chore: update memex abi

--- src/contract/abi/memex/ido/index.ts
+++ src/contract/abi/memex/ido/index.ts
@@ -4,6 +4,7 @@ import { memexIdoAbi0_1_17 } from './0.1.17'
 import { memexIdoAbi0_1_18 } from './0.1.18'
 import { memexIdoAbi0_1_19 } from './0.1.19'
 import { memexIdoAbi0_1_20 } from './0.1.20'
+import { memexIdoAbi0_1_21 } from './0.1.21'
 
 export const memexIdoAbiMap = {
   '0.1.15': memexIdoAbi0_1_15,
@@ -12,6 +13,7 @@ export const memexIdoAbiMap = {
   '0.1.18': memexIdoAbi0_1_18,
   '0.1.19': memexIdoAbi0_1_19,
   '0.1.20': memexIdoAbi0_1_20,
+  '0.1.21': memexIdoAbi0_1_21,
 }
 
 export type MemexIdoVersion = keyof typeof memexIdoAbiMap
