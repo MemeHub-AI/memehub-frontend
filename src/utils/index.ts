a9f0e835e44d888ec82686a6e96bc822b4952b35
l1en <2134045201@qq.com>
2024-08-24 10:35:26 +0800

    fix: joinPaths utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { first, includes, isEmpty } from 'lodash'
+import { first, isEmpty } from 'lodash'
 
 import { mediaLinks } from '@/config/link'
 
@@ -20,7 +20,10 @@ export const joinPaths = (...args: (string | number)[]) => {
   if (isEmpty(args)) return ''
 
   const firstHasSlash = first(args)?.toString().startsWith('/')
-  const path = args.map((a) => String(a).replace(/^\/|\/$/g, '')).join('/')
+  const path = args
+    .map((a) => String(a).replace(/^\/|\/$/g, ''))
+    .filter(Boolean)
+    .join('/')
 
   return firstHasSlash ? `/${path}` : path
 }
