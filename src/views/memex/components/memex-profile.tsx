306597876aeb46dfc1b51b7bd49b97f1acfa611b
qhcy <2407952148@qq.com>
2024-08-25 00:43:54 +0800

    feat: profile moblie adpatar

--- src/views/memex/components/memex-profile.tsx
+++ src/views/memex/components/memex-profile.tsx
@@ -1,30 +1,47 @@
-import DiamondIcon from '@/components/diamond-icon'
-import { Button } from '@/components/ui/button'
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from '@/components/ui/hover-card'
-import { Popover } from '@/components/ui/popover'
 import { useAccountContext } from '@/contexts/account'
 import { useClipboard } from '@/hooks/use-clipboard'
 import { useUser } from '@/hooks/use-user'
-import { Routes } from '@/routes'
-import FollowTab from '@/views/account/components/follow-tab'
-import ProfileForm from '@/views/account/components/profile-form'
-import {
-  EnvelopeClosedIcon,
-  HeartFilledIcon,
-  MinusIcon,
-  PlusIcon,
-} from '@radix-ui/react-icons'
-import { PopoverContent, PopoverTrigger } from '@radix-ui/react-popover'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'react-i18next'
-import { IoMdMore } from 'react-icons/io'
-import { IoCopyOutline, IoSettingsOutline } from 'react-icons/io5'
-import MemexAvatar from './memex-avatar'
 import { cn } from '@/lib/utils'
+import { UserInfoRes, UserUpdateReq } from '@/api/user/types'
+import { UseMutateAsyncFunction } from '@tanstack/react-query'
+import { ApiResponse } from '@/api/types'
+import AccountInfoDesktop from './account-info-desktop'
+import { useResponsive } from '@/hooks/use-responsive'
+import AccountInfoMoblie from './account-info-mobile'
+
+export interface AccountInfoProps {
+  userInfo: UserInfoRes | undefined
+  isOtherUser: boolean
+  isFollowing: boolean
+  isUnfollowing: boolean
+  tokenAddr: string
+  update: UseMutateAsyncFunction<
+    ApiResponse<UserInfoRes>,
+    Error,
+    UserUpdateReq,
+    string | number
+  >
+  follow: UseMutateAsyncFunction<
+    ApiResponse<UserInfoRes>,
+    Error,
+    string,
+    string | number
+  >
+  unfollow: UseMutateAsyncFunction<
+    ApiResponse<UserInfoRes>,
+    Error,
+    string,
+    string | number
+  >
+  refetchUserInfo: VoidFunction
+}
 
 export const MemexProfile = () => {
   const { userInfo, isOtherUser, refetchUserInfo, refetchFollow } =
@@ -36,149 +53,42 @@ export const MemexProfile = () => {
     },
   })
   const { t } = useTranslation()
-  const { copy } = useClipboard()
-  const { query, ...router } = useRouter()
+  const { query } = useRouter()
+  const { isPad } = useResponsive()
   const tokenAddr = (query.address || '') as string
 
-  const HoverCardPop: React.FC<{
-    children: React.ReactNode
-    content: string
-    variant?: 'start' | 'center' | 'end'
-    position?: 'top' | 'bottom' | 'left' | 'right'
-    className?: string
-  }> = ({
-    children,
-    content,
-    variant = 'center',
-    position = 'bottom',
-    className,
-  }) => (
-    <HoverCard>
-      <HoverCardTrigger asChild>{children}</HoverCardTrigger>
-      <HoverCardContent
-        className={cn(
-          'p-1 w-32 border-none text-center font-medium text-base',
-          className
-        )}
-        align={variant}
-        side={position}
-      >
-        {content}
-      </HoverCardContent>
-    </HoverCard>
-  )
-
-  // let rewardAmount = userInfo?.reward_amount
-  // console.log('11111111111111', typeof rewardAmount, rewardAmount)
-
   return (
-    <div className="flex-1 w-full rounded-lg border-2 border-zinc-200 overflow-hidden">
+    <div className="flex-1 w-full rounded-lg border-2 border-zinc-200">
       <div
         className="bg-cover bg-center h-72"
         style={{ backgroundImage: `url(/images/memex-profile-bg.jpg)` }}
       />
       <div className="bg-white px-2 pt-2 relative after:absolute after:w-full after:h-px after:bg-zinc-200 after:bottom-5 after:left-0">
-        <div className="w-full flex justify-between items-start">
-          <div className="flex space-x-4">
-            <MemexAvatar
-              userInfo={userInfo}
-              isOtherUser={isOtherUser}
-              update={update}
-              refetchUserInfo={refetchUserInfo}
-            />
-
-            <div>
-              <p className="font-bold text-2xl">{userInfo?.name}</p>
-              <FollowTab />
-
-              <div className="flex space-x-4 items-center">
-                <HoverCardPop content={t('account.total-likes')}>
-                  <span className="inline-flex items-center text-red-500">
-                    <HeartFilledIcon className="mr-1" />{' '}
-                    {userInfo?.like_count || 0}
-                  </span>
-                </HoverCardPop>
-
-                <HoverCardPop content={t('account.total-mentions')}>
-                  <span className="inline-flex items-center ml-1 text-black">
-                    <EnvelopeClosedIcon className="mr-1" />
-                    {userInfo?.mention_count || 0}
-                  </span>
-                </HoverCardPop>
-
-                <HoverCardPop
-                  content={t('reward.desc3')}
-                  variant="start"
-                  className="w-40"
-                >
-                  <span
-                    onClick={() => {
-                      if (isOtherUser) return
-                      router.push(Routes.Reward)
-                    }}
-                    className={cn(
-                      'flex items-center space-x-2',
-                      !isOtherUser &&
-                        'hover:underline-offset-1 hover:underline outline-black cursor-pointer'
-                    )}
-                  >
-                    <DiamondIcon size={17} />
-                    <span className="font-bold">
-                      {Number(userInfo?.reward_amount).toFixed(4) || 0}
-                    </span>
-                  </span>
-                </HoverCardPop>
-              </div>
-            </div>
-          </div>
-          <div className="flex space-x-6 items-center">
-            {isOtherUser ? (
-              <Button
-                variant={'purple'}
-                shadow={'none'}
-                className="flex items-center space-x-2"
-                disabled={isFollowing || isUnfollowing}
-                onClick={() =>
-                  userInfo?.is_follower
-                    ? unfollow(tokenAddr)
-                    : follow(tokenAddr)
-                }
-              >
-                {userInfo?.is_follower ? <MinusIcon /> : <PlusIcon />}
-                <span className="text-sm">
-                  {userInfo?.is_follower ? t('unfollow') : t('follow')}
-                </span>
-              </Button>
-            ) : (
-              <ProfileForm>
-                <Button
-                  variant={'purple'}
-                  shadow={'none'}
-                  className="flex items-center space-x-2"
-                >
-                  <IoSettingsOutline size={18} />
-                  <span className="text-sm">{t('edit')}</span>
-                </Button>
-              </ProfileForm>
-            )}
-
-            <Popover>
-              <PopoverTrigger>
-                <IoMdMore size={25} className="cursor-pointer" />
-              </PopoverTrigger>
-              <PopoverContent
-                side="left"
-                sideOffset={10}
-                align="start"
-                className="flex items-center gap-2 mt-10 w-52 rounded-md shadow-md shadow-zinc-300 bg-gray-50 text-black p-2 cursor-pointer hover:bg-zinc-100 text-sm border border-zinc-400"
-                onClick={() => copy(userInfo?.wallet_address ?? '')}
-              >
-                <IoCopyOutline size={17} />
-                <p>{t('copy.wallet.address')}</p>
-              </PopoverContent>
-            </Popover>
-          </div>
-        </div>
+        {!isPad ? (
+          <AccountInfoDesktop
+            userInfo={userInfo}
+            isOtherUser={isOtherUser}
+            isFollowing={isFollowing}
+            isUnfollowing={isUnfollowing}
+            tokenAddr={tokenAddr}
+            update={update}
+            follow={follow}
+            unfollow={unfollow}
+            refetchUserInfo={refetchUserInfo}
+          />
+        ) : (
+          <AccountInfoMoblie
+            userInfo={userInfo}
+            isOtherUser={isOtherUser}
+            isFollowing={isFollowing}
+            isUnfollowing={isUnfollowing}
+            tokenAddr={tokenAddr}
+            update={update}
+            follow={follow}
+            unfollow={unfollow}
+            refetchUserInfo={refetchUserInfo}
+          />
+        )}
       </div>
       <div className="align-top p-2 relative bottom-[0.65rem]">
         <p className="text-zinc-600">
@@ -191,3 +101,31 @@ export const MemexProfile = () => {
 }
 
 export default MemexProfile
+
+export const HoverCardPop = ({
+  children,
+  content,
+  variant = 'center',
+  position = 'bottom',
+  className,
+}: {
+  children: React.ReactNode
+  content: string
+  variant?: 'start' | 'center' | 'end'
+  position?: 'top' | 'bottom' | 'left' | 'right'
+  className?: string
+}) => (
+  <HoverCard>
+    <HoverCardTrigger asChild>{children}</HoverCardTrigger>
+    <HoverCardContent
+      className={cn(
+        'p-1 w-32 border-none text-center font-medium text-base',
+        className
+      )}
+      align={variant}
+      side={position}
+    >
+      {content}
+    </HoverCardContent>
+  </HoverCard>
+)
--- src/views/memex/profile/index.tsx
+++ src/views/memex/profile/index.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils'
 import { useUserInfo } from '@/hooks/use-user-info'
 import { AccountProvider } from '@/contexts/account'
 import { useRouter } from 'next/router'
-import { FollowTab } from '@/views/account/components/follow-tab'
+import { FollowDesktop } from '@/views/account/components/follow-desktop'
 import { AccountTab } from '@/views/account/components/account-tab'
 import { useUserList } from '@/views/account/hooks/use-user-list'
 import { UserListType } from '@/api/user/types'
@@ -43,7 +43,7 @@ export const MemexDetailsProfile = () => {
       }}
     >
       <MemexLayout>
-        <div className="flex-1 min-h-main flex gap-2 flex-col h-[calc(100vh-64px)] overflow-auto pl-2 pr-4 !ml-0">
+        <div className="flex-1 min-h-main flex gap-2 flex-col overflow-auto lg:pl-2 !ml-0">
           {/* Left aside */}
           <aside
             className={cn(
@@ -53,7 +53,7 @@ export const MemexDetailsProfile = () => {
           >
             <MemexProfile />
             <div className="hidden mt-4">
-              <FollowTab />
+              <FollowDesktop />
             </div>
           </aside>
 
