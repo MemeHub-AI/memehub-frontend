b06c5f7594b035e8db6823bec32f24760392812e
l1en <2134045201@qq.com>
2024-08-30 19:33:37 +0800

    feat: add max len tips

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,15 +6,45 @@ export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   disableFocusBorder?: boolean
   disableFocusShadow?: boolean
+  disableMaxLen?: boolean
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, disableFocusBorder, disableFocusShadow, ...props }, ref) => {
-    return (
+  (p, ref) => {
+    const {
+      className,
+      disableFocusBorder,
+      disableFocusShadow,
+      maxLength,
+      value,
+      disableMaxLen = false,
+      ...props
+    } = p
+
+    const withMaxLength = (children: React.ReactNode) => {
+      if (maxLength === undefined || disableMaxLen) return children
+      const currentLen = value?.toString().length || 0
+
+      return (
+        <div className="text-right">
+          {children}
+          <span
+            className={cn(
+              'text-zinc-500 text-sm leading-none',
+              currentLen >= maxLength && 'text-red-500'
+            )}
+          >
+            {currentLen}/{maxLength}
+          </span>
+        </div>
+      )
+    }
+
+    return withMaxLength(
       <textarea
         className={cn(
           'flex min-h-[60px] w-full rounded-md border border-input',
-          'bg-transparent px-3 py-2 text-sm shadow-sm',
+          'bg-transparent px-3 py-2 text-sm shadow-sm relative',
           'placeholder:text-muted-foreground focus-visible:outline-none',
           'disabled:cursor-not-allowed disabled:opacity-50',
           !disableFocusBorder && 'focus-visible:ring-1 focus-visible:ring-ring',
@@ -23,6 +53,8 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           className
         )}
         ref={ref}
+        value={value}
+        maxLength={maxLength}
         {...props}
       />
     )
