ff6f54189f96b37f7ea348a5cd5676050c2a3ccb
l1en <2134045201@qq.com>
2024-09-05 12:07:10 +0800

    feat: optimize idea scroll

--- src/views/memex/components/memex-infinite-scroll.tsx
+++ src/views/memex/components/memex-infinite-scroll.tsx
@@ -2,8 +2,6 @@ import { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
-import { memexBodyId } from './memex-tabs'
-
 export const MemexInfiniteScroll = <T,>({
   list,
   total,
@@ -22,7 +20,6 @@ export const MemexInfiniteScroll = <T,>({
       dataLength={list.length}
       next={fetchNext}
       hasMore={list.length < total}
-      scrollableTarget={memexBodyId}
       loader={<p className="text-center text-zinc-500 mt-2">{t('loading')}</p>}
       endMessage={
         <p className="text-center text-zinc-500 mt-2">{t('no-more')}</p>
