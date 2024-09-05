feefa45a8464153b4b1f5960325d12b83f349906
l1en <2134045201@qq.com>
2024-09-05 21:10:00 +0800

    chore: deploy

--- src/views/memex/latest/index.tsx
+++ src/views/memex/latest/index.tsx
@@ -1,10 +1,10 @@
 import { MemexIdeaCard } from '../components/idea-card'
 import { CustomSuspense } from '@/components/custom-suspense'
+import { MemexInfiniteScroll } from '../components/memex-infinite-scroll'
 import { IdeaEmpty } from '../components/idea-empty'
 import { IdeaCardSkeleton } from '../components/idea-card/skeleton'
 import { MemexListType } from '@/api/memex/types'
 import { useIdeaList } from '../hooks/use-idea-list'
-import { MemexInfiniteScroll } from '../components/memex-infinite-scroll'
 import { getMemexLayout } from '..'
 
 export const LatestPage = () => {
